8a39308667b18c00b1a9191f4513784bce1dcfe8
amachuca <amachuca@debian10.machuca>
2021-04-14 01:05:43 -0300

    responsive desing

--- js/app.js
+++ js/app.js
@@ -1,11 +1,38 @@
+// var icon = document.getElementById('icon');
+
+// icon.onclick = function() {
+//     console.log('modo oscuro');
+//     document.body.classList.toggle('dark-theme');
+//     if (document.body.classList.contains('dark-theme')) {
+//         icon.src = "image/sun.png";
+//     } else {
+//         icon.src = "image/moon.png";
+//     }
+// }
+
+var MenuItems = document.getElementById("MenuItems");
+
+MenuItems.style.maxHeight = "0px";
+function menutoggle() {
+    // body...
+    if (MenuItems.style.maxHeight == "0px") {
+        MenuItems.style.maxHeight = "200px"
+    } else {
+        MenuItems.style.maxHeight = "0px"
+    }
+}
+
+
 var icon = document.getElementById('icon');
+var menu = document.getElementById('menu-toggle')
 
 icon.onclick = function() {
-    console.log('modo oscuro');
     document.body.classList.toggle('dark-theme');
     if (document.body.classList.contains('dark-theme')) {
         icon.src = "image/sun.png";
+        menu.src = "image/menu-light.png"
     } else {
         icon.src = "image/moon.png";
+        menu.src = "image/menu.png"
     }
 }
